bdd7a408f409c5bd14eb27f2ea5353662e1edf32
Seth Zippel <zippelsf@uw.edu>
2016-11-29 15:03:11 -0800

    added function count_route_num, to identify individual ferry crossings

--- Data/data_download/ferry_data_utils.py
+++ Data/data_download/ferry_data_utils.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import xarray as xr
 from netCDF4 import Dataset
-
+from matplotlib import path
 
 def ferry_data_download(URL):
 	"""Downloads the ADCP data from ferry"""
@@ -61,4 +61,45 @@ def ferry_data_QC(ferry,TH_abs,TH_u,TH_d):
 	return(ferryQC)
 
 
+def count_route_num(ferryQc):
+	""" adds a variable to ferryQc dataset to mark the crossing number
 
+		INPUTS:
+			ferryQc - the xarray DataSet object with the ferry data
+		OUTPUTS:
+			ferryQc - xarray DataSet object with the ferry data, now with
+					  the added variable, xing_num
+	"""
+
+	#Define on-route box 
+	pt1 = (48.134302, -122.758195)
+	pt2 = (48.172176, -122.698912)
+	pt3 = (48.138532, -122.663936)
+	pt4 = (48.101688, -122.727816)
+	p = path.Path([pt1, pt2, pt3, pt4])
+
+	#check all lat/lon to be on or off route
+	on_route = p.contains_points( list(zip(ferry_small.latitude.values, 
+										   ferry_small.longitude.values)) );
+
+	#initialize vars
+	counter = 0;
+	xing_num = np.empty((ferry_small.time.size), dtype=int)
+
+	#Loop through all time values. If off route, give a -9 flag
+	# if on route, give it a route number. If it switches from off route to
+	# on route, increase the route number
+	for ii in range(1, ferry_small.time.size):
+	    lat = ferry_small.latitude[ii].values
+	    lon = ferry_small.longitude[ii].values
+	    
+	    if not(on_route[ii]):
+	        xing_num[ii] = -9
+	    else:
+	        xing_num[ii] = counter
+	    if  (on_route[ii] and not(on_route[ii-1])):
+	        counter = counter+1
+	xing_num = xr.DataArray(xing_num, coords=ferry_small.time.indexes, dims=['time'])
+	ferry_small['xing_num'] = xing_num
+
+    return(ferryQc)
