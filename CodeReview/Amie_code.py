da5411f3852efa6b2ba0640911ad96722f89edd3
abtadams <abtadams@uw.edu>
2016-11-30 12:12:55 -0800

    Updated Amie_code.py
    Added comments

--- CodeReview/Amie_code.py
+++ CodeReview/Amie_code.py
@@ -9,9 +9,12 @@ import numpy as np
 %matplotlib inline
 #Load data
 data = pd.read_csv('../Data/2014_PortTownsend.csv', parse_dates=['Date Time'])
+
 import plotly.plotly as py
 import plotly.graph_objs as go
+#Use date time index
 data_datetime = pd.DatetimeIndex(data['Date Time'])
+#Edit data adding new columns
 def f(data):
     data = data.copy()
     data['Year'] = pd.DatetimeIndex(data['Date Time']).year
@@ -21,14 +24,22 @@ def f(data):
     data['Minute'] = pd.DatetimeIndex(data['Date Time']).minute
     data['Time'] =  data['Hour'] + data['Minute']/60
     return data
+
 data1 = f(data)
+#Only grab 2014 data
 data2014 = data1.query('Year == 2014')
+#Only grab January data
 data012014 = data2014.query('Month == 1')
+#Only grab January first data
 data01012014 = data012014.query('Day == 1')
+#Edit data si it only has time and water depth data
 data01012014_simp = data01012014[['Time','Water Level']]
 data01012014_simp
+#Look fo local miniums and maxs
 data01012014_simp.sort_values(by=['Water Level', 'Time'], ascending=True)
+#Looking for a local minimum
 data01012014_simp[(data01012014_simp.Time >= 15) & (data01012014_simp.Time <= 20)]
+#Plot one day of data
 plt.style.use('ggplot')
 fig, ax = plt.subplots(figsize=(12, 6))
 plt.plot(data01012014['Time'], data01012014['Water Level'])
