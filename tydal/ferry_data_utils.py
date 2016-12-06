294472f55ce4ea1a575b6d37ec0be9fa446d82cb
Seth Zippel <zippelsf@uw.edu>
2016-12-06 14:10:35 -0800

    updated count_route_num to use time gaps, rather than location.

--- tydal/ferry_data_utils.py
+++ tydal/ferry_data_utils.py
@@ -61,44 +61,71 @@ def ferry_data_QC(ferry,TH_abs,TH_u,TH_d):
 	return(ferryQC)
 
 def count_route_num(ferryQc):
-	""" adds a variable to ferryQc dataset to mark the crossing number
+    """ adds a variable to ferryQc dataset to mark the crossing number
+         INPUTS:
+             ferryQc - the xarray DataSet object with the ferry data
+         OUTPUTS:
+             ferryQc - xarray DataSet object with the ferry data, now with
+                       the added variable, xing_num
+    """
+    #initialize vars
+    counter = 0
+    xing_num = np.empty((ferryQc.time.size), dtype=int)
+    tdiff = np.diff(ferryQc.time.values)
 
-		INPUTS:
-			ferryQc - the xarray DataSet object with the ferry data
-		OUTPUTS:
-			ferryQc - xarray DataSet object with the ferry data, now with
-					  the added variable, xing_num
-	"""
+    #Loop through all time values. Time gaps greater than 10mins indicate
+    #ferry docking, and the start of a new transect
+    for ii in range(1, ferryQc.time.size-1):
+        if tdiff[ii] > np.timedelta64(10, 'm'):
+            xing_num[ii] = -9
+            counter = counter+1
+        else:
+            xing_num[ii] = counter
+    #add to ferry structure
+    ferryQc['xing_num'] = xr.DataArray(xing_num,
+                                       coords=ferryQc.time.indexes,
+                                       dims=['time'])
+    return(ferryQc)
 
-	#Define on-route box 
-	pt1 = (48.134302, -122.758195)
-	pt2 = (48.172176, -122.698912)
-	pt3 = (48.138532, -122.663936)
-	pt4 = (48.101688, -122.727816)
-	p = path.Path([pt1, pt2, pt3, pt4])
+# def count_route_num(ferryQc):
+#     """ adds a variable to ferryQc dataset to mark the crossing number
 
-	#check all lat/lon to be on or off route
-	on_route = p.contains_points( list(zip(ferryQc.latitude.values, 
-										   ferryQc.longitude.values)) );
+#         INPUTS:
+#             ferryQc - the xarray DataSet object with the ferry data
+#         OUTPUTS:
+#             ferryQc - xarray DataSet object with the ferry data, now with
+#                       the added variable, xing_num
+#     """
+#     #Define on-route box 
+#     pt1 = (48.134302, -122.758195)
+#     pt2 = (48.172176, -122.698912)
+#     pt3 = (48.138532, -122.663936)
+#     pt4 = (48.101688, -122.727816)
+#     p = path.Path([pt1, pt2, pt3, pt4])
 
-	#initialize vars
-	counter = 0;
-	xing_num = np.empty((ferryQc.time.size), dtype=int)
+#     #check all lat/lon to be on or off route
+#     on_route = p.contains_points( list(zip(ferryQc.latitude.values, 
+#                                            ferryQc.longitude.values)) );
 
-	#Loop through all time values. If off route, give a -9 flag
-	# if on route, give it a route number. If it switches from off route to
-	# on route, increase the route number
-	for ii in range(1, ferryQc.time.size):
-	    lat = ferryQc.latitude[ii].values
-	    lon = ferryQc.longitude[ii].values
-	    
-	    if not(on_route[ii]):
-	        xing_num[ii] = -9
-	    else:
-	        xing_num[ii] = counter
-	    if  (on_route[ii] and not(on_route[ii-1])):
-	        counter = counter+1
+#     #initialize vars
+#     counter = 0;
+#     xing_num = np.empty((ferryQc.time.size), dtype=int)
 
-	xing_num = xr.DataArray(xing_num, coords=ferryQc.time.indexes, dims=['time'])
-	ferryQc['xing_num'] = xing_num
-	return(ferryQc)
+#     #Loop through all time values. If off route, give a -9 flag
+#     # if on route, give it a route number. If it switches from off route to
+#     # on route, increase the route number
+#     for ii in range(1, ferryQc.time.size):
+        
+#         if not(on_route[ii]):
+#             xing_num[ii] = -9
+#         else:
+#             xing_num[ii] = counter
+#         if  (on_route[ii] and not(on_route[ii-1])):
+#             counter = counter+1
+
+#     ferryQc['xing_num'] = xr.DataArray(xing_num,
+#     								   coords=ferryQc.time.indexes,
+#     								   dims=['time'])
+    
+#     return(ferryQc)
+
