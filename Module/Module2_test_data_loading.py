97e8934b6ba13d0398db37f9402260d90ea76ea5
Andrew Reed <reedan@uw.edu>
2016-12-09 11:41:46 -0800

    Adding unittesting for the loading data files

--- Module/Module2_test_data_loading.py
+++ Module/Module2_test_data_loading.py
@@ -3,21 +3,22 @@ import module2_utils as tides
 
 class TestDataLoading(unittest.TestCase):
 
-	# Testing the station map
-	def testNeahBayfails(self):
-		# Check that the data can't be loaded
-		self.assertRaises(FileNotFoundError,
+
+    # Testing the station map
+    def testNeahBayfails(self):
+        # Check that the data can't be loaded
+        self.assertRaises(FileNotFoundError,
                           tides.load_Neah_Bay('Bad Directory'))
 
 
     def testNeahBayloads(self):
-    	# Load the NeahBay data
+        # Load the NeahBay data
         NB = tides.load_Neah_Bay('../Data/')
         self.assertTrue(type(NB),pandas.core.frame.DataFrame)
 
 
     def testPortAngelesfails(self):
-    	# Check that the Port Angeles can't be loaded
+        # Check that the Port Angeles can't be loaded
         self.assertRaises(FileNotFoundError,
                           tides.load_Port_Angeles('Bad Directory'))
 
