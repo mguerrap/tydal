35e47989fa04415ed255b8da0b36d8cd39ca20ec
Seth Zippel <zippelsf@uw.edu>
2016-12-09 14:15:44 -0800

    testing branch issues

--- tydal/unit_test_module3.py
+++ tydal/unit_test_module3.py
@@ -14,16 +14,24 @@ class TestFerryPloting(unittest.TestCase):
         pt_tide = tu.load_Port_Townsend('../Data/')
         self.pt_tide = pt_tide['Water Level']
 
+        # import and clean ferry data
+        URL1 = ('http://107.170.217.21:8080/thredds/dodsC/Salish_L1_STA/' +
+                'Salish_L1_STA.ncml')
+        ferry = m3.ferry_data_download(URL1)
+        ferryQC = m3.ferry_data_QC(ferry)
+        self.ferryQC = m3.count_route_num(ferry)
+
         # define dates
         self.start_time = '2016-10-01'
         self.end_time = '2016-11-01'
-        self.time_index = 50000
+        self.time_index = 100
 
     # Test tide plotting subfunction
     def test_plt_tide(self):
         # Assert time_index is too large
+        time_index = 50000
         with self.assertRaises(ValueError):
-            m3.plt_tide(self.pt_tide, self.time_index,
+            m3.plt_tide(self.pt_tide, time_index,
                         self.start_time, self.end_time)
 
 
