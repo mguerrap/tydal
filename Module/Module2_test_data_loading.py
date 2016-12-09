b685e9bbbc41678bee69d034cf70545032233ce3
Andrew Reed <reedan@uw.edu>
2016-12-09 12:37:53 -0800

    Trying new testing methods

--- Module/Module2_test_data_loading.py
+++ Module/Module2_test_data_loading.py
@@ -1,4 +1,5 @@
 import unittest
+import pandas
 import module2_utils as tides
 
 class TestDataLoading(unittest.TestCase):
@@ -8,7 +9,7 @@ class TestDataLoading(unittest.TestCase):
     def testNeahBayfails(self):
         # Check that the data can't be loaded
         NB = tides.load_Neah_Bay('Bad Directory')
-        self.assertTrue(NB,None)
+        self.assertTrue(NB == None)
 
 
     def testNeahBayloads(self):
@@ -20,13 +21,13 @@ class TestDataLoading(unittest.TestCase):
     def testPortAngelesfails(self):
         # Check that the Port Angeles can't be loaded
         PA = tides.load_Port_Angeles('Bad Directory')
-        self.assertTrue(PA,None)
+        self.assertTrue(PA == None)
 
 
     def testPortAngelesloads(self):
         # Check that the Port Angeles will load
         PA = tides.load_Port_Angeles('../Data/')
-        self.assertIS(type(PA),pandas.core.frame.DataFrame)
+        self.assertIs(type(PA),pandas.core.frame.DataFrame)
 
 
 if __name__ == '__main__':
