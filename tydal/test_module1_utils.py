56b7e55fca429dc64492ea0c58cba0143970a3ba
zwescoe <zakwescoe@gmail.com>
2016-12-12 20:43:22 -0800

    Started unit tests for module1.

---
+++ tydal/test_module1_utils.py
@@ -0,0 +1,19 @@
+import unittest
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import module1_utils as mod1
+
+class TestMod1(unittest.TestCase):
+    """
+    Creates a class to test the functions in module 1 utilities.
+    """
+
+    def test_trim_data(self):
+        """
+        This function tests the "trim_data" function.
+        """
+
+
+if __name__ == "__main__":
+    unittest.main()
