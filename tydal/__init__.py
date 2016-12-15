9b96dbf80e48323d941133e7ed8efb56b5264d3d
zwescoe <zakwescoe@gmail.com>
2016-12-14 23:13:15 -0800

    Updated init to contain all py files.

--- tydal/__init__.py
+++ tydal/__init__.py
@@ -1,1 +1,5 @@
-__all__ = ['data_get_2014', 'data_get_year', 'ferry_data_utils', 'module1_utils', 'module2_utils', 'module3_utils', 'MoonCycleDates', 'plot_currents', 'tidal_currents', 'unit_test_module3', 'tide_utils']
+__all__ = ['module1_fancy_graphs', 'data_get_year', 'ferry_data_utils',
+           'module1_utils', 'module2_utils', 'module3_utils', 
+           'MoonCycleDates', 'plot_currents', 'tidal_currents',
+           'quiz1.py', 'quiz2.py', 'quiz3.py', 'test_module1_utils',
+           'test_module2_utils', 'test_module3_utils',]
